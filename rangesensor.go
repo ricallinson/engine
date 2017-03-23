6d2e706441ce50a111f4ee530595dce4139ee7b6
Ric Allinson <ric@ricallinson.com>
2017-03-22 20:20:39 -0700

    Change echo pin to 25 as a test

--- rangesensor.go
+++ rangesensor.go
@@ -43,9 +43,11 @@ func (this *RangeSensor) Get() float32 {
 	// The HC-SR04 sensor requires a short 10uS pulse to trigger the module, which will
 	// cause the sensor to start the ranging program (8 ultrasound bursts at 40 kHz) in
 	// order to obtain an echo response. So, to create our trigger pulse, we set out
-	// trigger pin high for 10uS then set it low again.
+	// trigger pin high for 10uS then set it low again. Send a short rpio.Low pulse just to make sure.
+	this.pinTrigger.Low()
+	time.Sleep(5 * time.Microsecond)
 	this.pinTrigger.High()
-	time.Sleep(1 * time.Millisecond)
+	time.Sleep(10 * time.Microsecond)
 	this.pinTrigger.Low()
 	// Measure the distance.
 	distance := this.takeMeasurement()
