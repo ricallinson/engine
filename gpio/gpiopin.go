b2470df0d4f19d14849c4f9d44afaa348ead249a
Ric Allinson <ric@ricallinson.com>
2017-03-25 11:25:13 -0700

    Reworking PWM

--- gpio/gpiopin.go
+++ gpio/gpiopin.go
@@ -147,7 +147,7 @@ func (this *GpioPin) GetModulation() int {
 // Software implemented Pulse Width Modulation (PWM) at every 2ms.
 func (this *GpioPin) modulateGpioPin() {
 	width := 100 // in uS - this should be 200uS but in testing something is off.
-	widthHighOffset := 50
+	widthHighOffset := 40
 	var high int
 	var low int
 	// Check that modulation value is in range.
@@ -160,6 +160,6 @@ func (this *GpioPin) modulateGpioPin() {
 		time.Sleep(time.Duration(high-widthHighOffset) * time.Microsecond)
 		this.Low()
 		// Sleep for pulse low duration.
-		time.Sleep(time.Duration(low) * time.Microsecond)
+		time.Sleep(time.Duration(low+widthHighOffset) * time.Microsecond)
 	}
 }
