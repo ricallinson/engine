b761a668dbe110e6b60eb787f3f120c512f4c3ff
Ric Allinson <ric@ricallinson.com>
2017-03-25 01:04:56 -0700

    Changed led-pwm example

--- examples/led-pwm/main.go
+++ examples/led-pwm/main.go
@@ -20,13 +20,13 @@ func main() {
 	var led = robot.NewLED(2)
 
 	// Create a variable to hold the brightness percentage.
-	var brightness float32 = 0.10
+	var brightness float32 = 0
 
-	// Run a loop for 10 times, each time increasing the brightness of the LED by 10%.
-	for x := 0; x < 10; x++ {
+	// Run a loop for 100 times, each time increasing the brightness of the LED by 1%.
+	for x := 0; x < 100; x++ {
 		led.Set(brightness)
-		brightness = brightness + 0.10
-		time.Sleep(time.Second * 2)
+		brightness = brightness + 0.01
+		time.Sleep(time.Second / 2)
 	}
 
 	// Turn the LED off before exiting the program.
