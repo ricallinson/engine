3336ddd8eea696214f866849ba714f312fa1346d
Ric Allinson <allinson@yahoo-inc.com>
2017-04-18 09:30:30 -0700

    Bug fixes from the real test

--- examples/line-follower/main.go
+++ examples/line-follower/main.go
@@ -17,10 +17,10 @@ func main() {
 	var robot = engine.Start(false)
 
 	// Create a new instance of a Motor for the left wheel.
-	var motorLeft = robot.NewMotor(6, 13, 19, true)
+	var motorLeft = robot.NewMotor(6, 13, 19, false)
 
 	// Create a new instance of a Motor for the right wheel.
-	var motorRight = robot.NewMotor(16, 20, 21, false)
+	var motorRight = robot.NewMotor(16, 20, 21, true)
 
 	// Create a new instance of an IRSensor for the left of the line.
 	var lineSensorLeft = robot.NewIRSensor(8)
@@ -31,24 +31,24 @@ func main() {
 	// Start a loop that will run endlessly.
 	for {
 		// This will make the robot move in a straight line.
-		motorLeft.Forwards()
-		motorRight.Forwards()
+		motorLeft.Set(0.6)
+		motorRight.Set(0.6)
 
 		// Check if the left senors has a value.
-		if lineSensorLeft.Get() < 1 {
+		if lineSensorLeft.Get() > 0 {
 			// If it does then the robot needs to move to the left.
-			motorLeft.Stop()
+			motorRight.Stop()
 		}
 
 		// Check if the left senors has a value.
-		if lineSensorRight.Get() < 1 {
+		if lineSensorRight.Get() > 0 {
 			// If it does then the robot needs to move to the right.
-			motorRight.Stop()
+			motorLeft.Stop()
 		}
 
 		// All the above happen in less than 1,000s of a second.
 		// To give the robot time to move we put the code to sleep for 1/4 of second.
-		time.Sleep(time.Second / 4)
+		time.Sleep(time.Second / 200)
 	}
 
 	// At the end of the program it's good practice to stop the processing engine.
